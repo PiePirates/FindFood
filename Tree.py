5cf7c9aacc104f6672a900bbea9679e73c66d61b
PiePirates <111090985+PiePirates@users.noreply.github.com>
2022-09-19 19:00:45 -0500

    Add files via upload
    Added basic tree structure

---
+++ Tree.py
@@ -0,0 +1,14 @@
+class Tree:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+    
+    def add_child(self, child_node):
+        self.children.append(child_node)
+    
+    def remove_child(self, child_node):
+        self.children = [child for child  in self.children if child != child_node]
+
+
+
+    
